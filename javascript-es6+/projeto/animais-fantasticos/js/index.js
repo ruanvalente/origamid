82cda885fcc9842c4ca3941e2e27e9f10fcc29a7
ruanvalente <ruanopensuselinux@gmail.com>
2019-07-17 15:20:45 -0300

    Aula: 0309 Navegação por Tabs. Finalizada.

---
+++ javascript-es6+/projeto/animais-fantasticos/js/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const $tabMenu = document.querySelectorAll('.js-tabmenu li')
+const $tabContent = document.querySelectorAll('.js-tabcontent section')
+
+$tabContent[0].classList.add('ativo')
+
+function activeTab(index) {
+  $tabContent.forEach(content => content.classList.remove('ativo'))
+  $tabContent[index].classList.add('ativo')
+}
+
+$tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index)
+  })
+})
