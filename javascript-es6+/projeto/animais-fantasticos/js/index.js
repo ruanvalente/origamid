d24fd7a0c172f5db374107afa89c30d5bb9eee1f
ruanvalente <ruanopensuselinux@gmail.com>
2019-07-31 15:32:39 -0300

    Adicionado o modulo initModal

--- javascript-es6+/projeto/animais-fantasticos/js/index.js
+++ javascript-es6+/projeto/animais-fantasticos/js/index.js
@@ -1,11 +1,11 @@
-'use strict'
-
 import initScrollSection from './modules/initScrollSection.js'
 import initScroll from './modules/initScroll.js'
 import initTab from './modules/initTab.js'
 import initAccordion from './modules/initAccordion.js'
+import initModal from './modules/initModal.js'
 
 initScrollSection()
 initScroll()
 initTab()
 initAccordion()
+initModal()
