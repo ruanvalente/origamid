7190e944a5426501aeca614dc3f9e910b2d1cb87
ruanvalente <ruanopensuselinux@gmail.com>
2019-08-01 15:02:06 -0300

    Adicionado o modulo initTooltip no projeto

--- javascript-es6+/projeto/animais-fantasticos/js/index.js
+++ javascript-es6+/projeto/animais-fantasticos/js/index.js
@@ -3,9 +3,11 @@ import initScroll from './modules/initScroll.js'
 import initTab from './modules/initTab.js'
 import initAccordion from './modules/initAccordion.js'
 import initModal from './modules/initModal.js'
+import initTooltip from './modules/initTooltip.js'
 
 initScrollSection()
 initScroll()
 initTab()
 initAccordion()
 initModal()
+initTooltip()
