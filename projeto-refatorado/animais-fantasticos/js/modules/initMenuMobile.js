0f19196eec10cf13ff363327678518af239e02b9
ruanvalente <ruanopensuselinux@gmail.com>
2019-09-10 22:15:13 -0300

    Refatorado o modulo initMenuMobile

--- projeto-refatorado/animais-fantasticos/js/modules/initMenuMobile.js
+++ projeto-refatorado/animais-fantasticos/js/modules/initMenuMobile.js
@@ -1,20 +1,33 @@
 import outSideClick from './initOutside.js';
 
-export default function initMenuMobile() {
-  const $buttonMenu = document.querySelector('[data-js="button-menu"]');
-  const $listMenu = document.querySelector('[data-js="list-menu"]');
-  const events = ['click', 'touchstart'];
+export default class MenuMobile {
+  constructor(buttonMenu, listMenu, events) {
+    this.$buttonMenu = document.querySelector(buttonMenu);
+    this.$listMenu = document.querySelector(listMenu);
+    this.events = [...events];
+    this.handleOpenMenu = this.handleOpenMenu.bind(this);
+  }
 
-  function openMenu() {
-    $buttonMenu.classList.add('active');
-    $listMenu.classList.add('active');
+  handleOpenMenu() {
+    this.$buttonMenu.classList.add('active');
+    this.$listMenu.classList.add('active');
 
-    outSideClick($listMenu, events, () => {
-      $buttonMenu.classList.remove('active');
-      $listMenu.classList.remove('active');
+    outSideClick(this.$listMenu, this.events, () => {
+      this.$buttonMenu.classList.remove('active');
+      this.$listMenu.classList.remove('active');
     });
   }
-  events.forEach(userEvents =>
-    $buttonMenu.addEventListener(userEvents, openMenu)
-  );
+
+  addButtonMenuEvents() {
+    this.events.forEach(userEvents =>
+      this.$buttonMenu.addEventListener(userEvents, this.handleOpenMenu)
+    );
+  }
+
+  init() {
+    if (this.$buttonMenu && this.$listMenu) {
+      this.addButtonMenuEvents();
+    }
+    return this;
+  }
 }
