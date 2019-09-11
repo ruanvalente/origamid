1149dedef7e7e9b4cc51af392b442205da129a33
ruanvalente <ruanopensuselinux@gmail.com>
2019-09-11 16:16:15 -0300

    Realizado os calculos de distancias de quando cada evento ocorre

--- projeto-slide/js/slide.js
+++ projeto-slide/js/slide.js
@@ -2,21 +2,38 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.distances = {
+      finalPosition: 0,
+      startPosition: 0,
+      movementPosition: 0
+    };
+  }
+
+  movieSlide(distX) {
+    this.distances.moviePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePostion(clientX) {
+    this.distances.movementPosition =
+      (this.distances.startPosition - clientX) * 1.6;
+    return this.distances.finalPosition - this.distances.movementPosition;
   }
 
   onStart(e) {
     e.preventDefault();
+    this.distances.startPosition = e.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
-    console.log(e);
   }
 
   onMove(e) {
-    console.log('move');
+    const finalPosition = this.updatePostion(e.clientX);
+    this.movieSlide(finalPosition);
   }
 
   onEnd(e) {
-    console.log('acabou');
     this.wrapper.removeEventListener('mousedown', this.onMove);
+    this.distances.finalPosition = this.distances.moviePosition;
   }
 
   addSlideEvents() {
